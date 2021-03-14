1cf3b98ba76e23ddc0421e3e4427602876ee3e8f
Neythen <zcqsntr@ucl.ac.uk>
2021-03-14 11:15:18 +0000

    scaled up start screen

--- run_retro_matrix.cc
+++ run_retro_matrix.cc
@@ -34,7 +34,129 @@
 
 
 
+void draw_logo(Canvas* canvas, Point pos, int scale, Color blue, Color white) {
+  
+    
+    
+    
+    
+    vector<vector<Point>> blues;
+    vector<Point> whites;
+    
+    
+    
+   
+    for(int i = 0; i < 12*scale; i++){
+      vector<Point> ps;
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+            whites.push_back(Point{pos.row + 13*scale - i+row, pos.col +27*scale +col});
+        }
+      }
+      
+      //whites.push_back(ps);
+      
+      //blues.push_back({Point{pos.row + i, pos.col +25}, Point{pos.row + i, pos.col +26}, Point{pos.row + i, pos.col +28}, Point{pos.row + i, pos.col +29}});
+    }
+    
+    for(int i = 0; i < 7*scale; i++){
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+            whites.push_back(Point{pos.row + 2*scale + i - row, pos.col + 27*scale- i + col});
+        }
+      }
+        
+      
+    }
+    
+    for(int i = 0; i < 7*scale; i++){
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+            if(!(i ==0 && row == scale -1 && col == scale-1)) {
+              whites.push_back(Point{pos.row + 9*scale +row - i, pos.col + 20*scale +col - i});
+            }
+        }
+      }
+    }
+    
+    for (int i = 0; i < 12*scale; i++ ){
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+            if(!(i == 12*scale -1 && row == scale -1)){
+              whites.push_back(Point{pos.row + 2*scale + i +row, pos.col + 13*scale + col});
+            }
+            
+      }
+    }
+    }
+    
+    for (int i = 0; i < 11*scale; i++ ){
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+        whites.push_back(Point{pos.row + 13*scale - i +row, pos.col + 13*scale - i + col});
+      }
+    }
+    }
+    
+    for (int i = 0; i < 14*scale; i++ ){
+      for(int row = 0; row < scale; row++){
+          for(int col = 0; col<scale; col++){
+            if (pos.row + 2*scale +row + i <= pos.row +15*scale) {
+            whites.push_back(Point{pos.row + 2*scale +row + i, pos.col + 2*scale + col});
+          }
+      }
+    }
+    }
+    
+    
+    
+    
 
+  
+      
+      for(auto point: whites){
+        vector<Point> ps;
+      
+        
+        for (int row = -2*scale; row < 2*scale+1; row++){
+          for(int col = -2*scale; col < 2*scale+1; col++){
+            if(row*row + col*col <= 4*scale*scale +1 && point.row + row <= pos.row + 15*scale) {
+              ps.push_back(Point{point.row + row, point.col + col});
+            }
+          }
+        }
+        blues.push_back(ps);
+      }
+      
+      
+    
+    
+
+    for(int i = 0; i < blues.size(); i++){
+      for(const Point point: blues[i]) {
+        
+        // only set the blue pixels that havent already been set to white
+        bool set = true;
+        for (int j = 0; j<i; j++){
+          
+          
+          if(whites[j].row == point.row && whites[j].col == point.col) {
+            set = false;
+          }
+
+        }
+        
+        if (set) {
+          SetPixel(canvas, point, blue);
+        }
+      }
+      
+      
+      SetPixel(canvas, whites[i],white);
+      usleep(10/(scale*scale) * 1000);
+    }
+  
+} 
 
 int main(int argc, char *argv[]) {
   RGBMatrix::Options defaults; 
@@ -78,8 +200,42 @@ int main(int argc, char *argv[]) {
   while(true){
     // ps4 controller "/dev/input/event6",
     connected_contr = 0;
+    
+    Color blue(100*matrix.brightness,150*matrix.brightness,255*matrix.brightness);
+    Color white(255*matrix.brightness,255*matrix.brightness,255*matrix.brightness);
+    
+    Point pos = Point{4,2};
+    int scale = 2;
+    draw_logo(canvas, pos, scale, blue, white);
+    
+    
+    rgb_matrix::Font brand_font;
+    if (!brand_font.LoadFont("./rpi-rgb-led-matrix/fonts/5x7.bdf")) {
+      fprintf(stderr, "Couldn't load font \n");
+    
+    }
+    int letter_spacing = 0;
+    Color c1(150*matrix.brightness, 0, 255*matrix.brightness);
+    Color c2(70*matrix.brightness, 0, 150*matrix.brightness);
+    draw_text(canvas, brand_font, 38 + brand_font.baseline(),  5, blue, Color{0,0,0}, "NEYTH MAKES", letter_spacing);
+    
+    Point start_pos = Point{42, 10};
+     rgb_matrix::Font font;
+    if (!font.LoadFont("./rpi-rgb-led-matrix/fonts/4x6.bdf")) {
+      fprintf(stderr, "Couldn't load font \n");
+    
+    }
+    int frame = 0;
+    bool bright = true;
     while (connected_contr==0) { // while controller disconnected 
-       
+        if (frame%1000000 == 0) {
+         if(bright) {
+            draw_text(canvas, font, start_pos.row + 3*font.baseline(), start_pos.col, c1 , Color{0,0,0}, "PRESS START!", letter_spacing);
+          } else {
+            draw_text(canvas, font, start_pos.row + 3*font.baseline(), start_pos.col, c2 , Color{0,0,0}, "PRESS START!", letter_spacing);
+          }
+          bright = !bright;
+        }
         for (const auto & entry : std::filesystem::directory_iterator(path)){
          
           if(regex_match((char *)entry.path().c_str(), regex("/dev/input/event[0-9]+")) && !count(devices.begin(), devices.end(), ((string)entry.path()).back())){ //check if device has already been looked at
@@ -157,121 +313,17 @@ int main(int argc, char *argv[]) {
     cout << "conts: " <<connected_contr<<endl;
     
     
-    Color white(255,255,255);
-    Color blue(100,150,255);
-    
-    Point pos = Point{5,18};
-    
-    vector<vector<Point>> blues;
-    vector<Point> whites;
-       
+   
     
     
-    
-    blues.push_back({Point{pos.row +15,pos.col +26}, Point{pos.row +15,pos.col +27}, Point{pos.row +15, pos.col +28}});
-    blues.push_back({Point{pos.row +14, pos.col +25}, Point{pos.row +14,pos.col +26}, Point{pos.row +14,pos.col +27}, Point{pos.row +14, pos.col +28}, Point{pos.row +14, pos.col +29}});
-    
-    for(int i = 13; i >1; i--){
-      whites.push_back(Point{pos.row + i, pos.col +27});
-      //blues.push_back({Point{pos.row + i, pos.col +25}, Point{pos.row + i, pos.col +26}, Point{pos.row + i, pos.col +28}, Point{pos.row + i, pos.col +29}});
-    }
-    
-    for(int i = 0; i < 7; i++){
-      whites.push_back(Point{pos.row + 3 + i, pos.col + 26 - i});
-    }
-    
-    for(int i = 0; i < 6; i++){
-      whites.push_back(Point{pos.row + 8 - i, pos.col + 19 - i});
-    }
-    
-    for (int i = 0; i < 12; i++ ){
-      whites.push_back(Point{pos.row + 2 + i, pos.col + 13});
-    }
-    
-    for (int i = 0; i < 11; i++ ){
-      whites.push_back(Point{pos.row + 13 - i, pos.col + 13 - i});
-    }
-    
-    for (int i = 0; i < 14; i++ ){
-      whites.push_back(Point{pos.row + 2 + i, pos.col + 2});
-    }
-    
-    for(int i = 0; i < 2; i++){
-      for(auto point: blues[i]) {
-        SetPixel(canvas, point, blue);
-      }
-      usleep(25 * 1000);
-    }
-    
-    for(int i = 0; i < whites.size() - 2; i++) {
-      Point point = whites[i];
-      
-                      
-      if(i == whites.size()-3) {
-        blues.push_back({Point{point.row -1, point.col }, Point{point.row-2, point.col }, Point{point.row -1, point.col-1 }, Point{point.row-2, point.col-1 }, Point{point.row -1, point.col+1 }, Point{point.row-2, point.col+1 }, 
-                        Point{point.row +1, point.col }, Point{point.row+2, point.col }, Point{point.row +1, point.col-1 }, Point{point.row+2, point.col-1 }, Point{point.row +1, point.col+1 }, Point{point.row+2, point.col+1 }, 
-                        Point{point.row, point.col-1 }, Point{point.row , point.col-2 }, Point{point.row +1, point.col-2 }, Point{point.row-1 , point.col-2 },
-                        Point{point.row, point.col+1 }, Point{point.row , point.col+2 }, Point{point.row +1, point.col+2 }, Point{point.row-1 , point.col+2 }, Point{point.row+2 , point.col+2 }, Point{point.row+2 , point.col-2 }
-                      });
-      } else {
-        blues.push_back({Point{point.row -1, point.col }, Point{point.row-2, point.col }, Point{point.row -1, point.col-1 }, Point{point.row-2, point.col-1 }, Point{point.row -1, point.col+1 }, Point{point.row-2, point.col+1 }, 
-                        Point{point.row +1, point.col }, Point{point.row+2, point.col }, Point{point.row +1, point.col-1 }, Point{point.row+2, point.col-1 }, Point{point.row +1, point.col+1 }, Point{point.row+2, point.col+1 }, 
-                        Point{point.row, point.col-1 }, Point{point.row , point.col-2 }, Point{point.row +1, point.col-2 }, Point{point.row-1 , point.col-2 },
-                        Point{point.row, point.col+1 }, Point{point.row , point.col+2 }, Point{point.row +1, point.col+2 }, Point{point.row-1 , point.col+2 }
-                      });
-      }
-      
-    }
-    
-
-    for(int i = 0; i < blues.size(); i++){
-      for(const Point point: blues[i]) {
-        
-        // only set the blue pixels that havent already been set to white
-        bool set = true;
-        for (int j = 0; j<i; j++){
-          if(whites[j].row == point.row && whites[j].col == point.col) {
-            set = false;
-          }
-
-        }
-        
-        if (set) {
-          SetPixel(canvas, point, blue);
-        }
-      }
-      
-      
-      SetPixel(canvas, whites[i],white);
-      usleep(10 * 1000);
-    }
-    
-    rgb_matrix::Font brand_font;
-    if (!brand_font.LoadFont("./rpi-rgb-led-matrix/fonts/5x7.bdf")) {
-      fprintf(stderr, "Couldn't load font \n");
-    
-    }
-    int letter_spacing = 0;
-    Color c1(150, 0, 255);
-    Color c2(70, 0, 150);
-    draw_text(canvas, brand_font, 23 + brand_font.baseline(),  5, blue, Color{0,0,0}, "NEYTH MAKES", letter_spacing);
-    
-    rgb_matrix::Font font;
-    if (!font.LoadFont("./rpi-rgb-led-matrix/fonts/4x6.bdf")) {
-      fprintf(stderr, "Couldn't load font \n");
-    
-    }
-    
-    int frame = 0;
-    bool bright = true;
     while (connected_contr>0)  {
  
       if (frame%1000000 == 0) {
         
         if(bright) {
-          draw_text(canvas, font, 32 + 3*font.baseline(), 10, c1 , Color{0,0,0}, "PRESS START!", letter_spacing);
+          draw_text(canvas, font, start_pos.row  + 3*font.baseline(), start_pos.col , c1 , Color{0,0,0}, "PRESS START!", letter_spacing);
         } else {
-          draw_text(canvas, font, 32 + 3*font.baseline(), 10, c2 , Color{0,0,0}, "PRESS START!", letter_spacing);
+          draw_text(canvas, font, start_pos.row  + 3*font.baseline(), start_pos.col, c2 , Color{0,0,0}, "PRESS START!", letter_spacing);
         }
         bright = !bright;
       }
